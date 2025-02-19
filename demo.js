966b4e6b53c0a55c8ff6d61c9474ed51e5877d7e
朴睦 <927561237@qq.com>
2025-02-19 09:45:50 +0800

    两个顺序数组，合并为一个顺序数组。双指针法

---
+++ demo.js
@@ -0,0 +1,73 @@
+function transformObject(obj) {
+  const result = {};
+  // 遍历原始对象的每个键值对
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const value = obj[key];
+      // 将键按点号分割成键路径数组
+      const keyPath = key.split('.');
+      let currentLevel = result;
+      // 遍历键路径数组
+      for (let i = 0; i < keyPath.length; i++) {
+        const currentKey = keyPath[i];
+        if (i === keyPath.length - 1) {
+          // 如果是最后一个键，直接赋值
+          currentLevel[currentKey] = value;
+        } else {
+          // 如果不是最后一个键，检查该键对应的对象是否存在
+          if (!currentLevel[currentKey]) {
+            // 若不存在，创建一个空对象
+            currentLevel[currentKey] = {};
+          }
+          // 移动到下一层对象
+          currentLevel = currentLevel[currentKey];
+        }
+      }
+    }
+  }
+  return result;
+}
+
+const obj = {
+  "a.b.c": 123,
+  "a.b.d": 124,
+  "d.a.c": 432
+};
+
+const transformedObj = transformObject(obj);
+console.log(transformedObj); // { a: { b: { c: 123, d: 124 } }, d: { a: { c: 432 } } }
+
+/**
+ * 两个顺序数组，合并为一个顺序数组。双指针法
+ */
+const arr1 = [1, 7, 8, 9]
+const arr2 = [3, 4, 6]
+function mergeArr(arr1, arr2) {
+  // 双指针
+  let i = 0
+  let j = 0
+  const arr = []
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      arr.push(arr1[i])
+      i++
+    } else {
+      arr.push(arr2[j])
+      j++
+    }
+  }
+
+  while (i < arr1.length) {
+    arr.push(arr1[i])
+    i++
+  }
+
+  while (j < arr2.length) {
+    arr.push(arr2[j])
+    j++
+  }
+
+  return arr
+}
+console.log(mergeArr(arr1, arr2));
