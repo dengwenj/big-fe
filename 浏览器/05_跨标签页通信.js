1122306500d9720ff9c46e2e372aed113cd04f4d
朴睦 <927561237@qq.com>
2025-02-25 22:39:42 +0800

    跨标签页通信

---
+++ 浏览器/05_跨标签页通信.js
@@ -0,0 +1,10 @@
+/**
+ * 1、BroadCast Channel（广播）
+ * 2、Service Worker
+ * 3、LocalStorage window.onstorage 监听(将值存储到本地存储时，会触发这个事件) localStorage.setItem('key', value)
+ * 4、Shared Worker 定时器轮询
+ * 5、IndexedDB 定时器轮询
+ * 6、cookie 定时器轮询
+ * 7、window.open、window.postMessage
+ * 8、Websocket
+ */
