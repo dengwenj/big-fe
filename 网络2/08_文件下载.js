e1eb3ee40830ca059a404a420e985b892c842bee
朴睦 <927561237@qq.com>
2025-03-01 15:48:53 +0800

    session

---
+++ 网络2/08_文件下载.js
@@ -0,0 +1,4 @@
+/**
+ * 文件下载的消息格式
+ * 服务器只要在响应头中加入 Content-Disposition: attachment; filename="xxx" 即可触发浏览器的下载功能
+ */
---
+++ 网络2/09_session.js
@@ -0,0 +1,11 @@
+/**
+ * cookie 和 session 的区别
+ * 1、cookie 的数据保存在浏览器端，session 的数据保存在服务器
+ * 2、cookie 的存储空间有限，session 的存储空间不限
+ * 3、cookie 只能保存字符串，session 可以保存任何类型的数据
+ * 4、cookie 中的数据容易被获取，session 中的数据难以获取
+ * 
+ * 如何消除 session
+ * 1、设置过期时间
+ * 2、客户端主动通知
+ */
