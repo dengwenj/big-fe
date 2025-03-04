cfe110d79613455bd4776525500991a3c031b8f0
朴睦 <927561237@qq.com>
2025-03-05 00:47:32 +0800

    简易版防抖和 message弹出信息-> 最多只能同时执行n个任务,当某个任务到达时间后添加其他任务

--- demo.js
+++ demo.js
@@ -71,3 +71,98 @@ function mergeArr(arr1, arr2) {
   return arr
 }
 console.log(mergeArr(arr1, arr2));
+
+
+// 防抖
+function debounce(fn, time) {
+  let timer = null
+  return function(...args) {
+    if (timer) {
+      // 清除上一次的
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, time)
+  }
+}
+
+const fn = debounce((num1, num2) => {
+  console.log(num1, num2)
+}, 100)
+// 调用这个函数，会触发回调函数
+fn(1, 2)
+
+setTimeout(() => {
+  fn()
+}, 140)
+
+setTimeout(() => {
+  fn(3, 4)
+}, 30)
+
+// 多任务同时执行，一个一个进 有个任务的最大值，当某个任务执行完后，继续执行下一个任务
+class Task {
+  constructor(maxNum, time) {
+    // 保存同时最多能执行多个任务
+    this.maxNum = maxNum
+    // 任务要执行多久
+    this.time = time
+    // 存放任务的队列
+    this.tasks = []
+    // 执行任务的队列
+    this.runTasks = []
+  }
+
+  // time: 任务执行的时间是多久
+  run() {
+    while (this.maxNum > this.runTasks.length) {
+      const { fn, time } = this.tasks.shift()
+      this.runTasks.push(fn)
+      setTimeout(() => {
+        fn.call(this)
+        this.runTasks = this.runTasks.filter((item) => item !== fn)
+
+        if (this.tasks.length) {
+          this.run()
+        }
+      }, time)
+    }
+  }
+
+  addTask(fn, time) {
+    this.tasks.push({
+      fn,
+      time
+    })
+  }
+}
+
+const t = new Task(3, 1000)
+
+t.addTask(function t1() {
+  console.log("任务1")
+}, 1000)
+
+t.addTask(function t2() {
+  console.log("任务2");
+}, 1000)
+
+t.addTask(function t3() {
+  console.log("任务3")
+}, 1000)
+
+t.addTask(function t4() {
+  console.log("任务4")
+}, 1000)
+
+t.addTask(function t6() {
+  console.log("任务6")
+}, 1000)
+
+
+t.addTask(function t5() {
+  console.log("任务5")
+}, 1000)
+
+t.run()
